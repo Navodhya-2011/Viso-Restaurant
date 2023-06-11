44adaf0af47a9fc022aed3eebf2c235fe7f416d4
Navodhya Yasisuru <navodhya2011@gmail.com>
2023-06-11 08:42:26 +0530

    Commit Changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home1 from "pages/Home1/index";
+import Menu from "pages/Login/index";
+import OurTeam from "pages/Menu/index";
+import Signup from "pages/Signup/index";
+import Login from "pages/Login/index";
+
 const Home = () => {
   return (
     <Router>
         <Routes>
         <Route path="/" element={<Home1 />} />
+        <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/home1" element={<Home1 />} />
+          <Route path="/ourteam" element={<OurTeam />} />
+          <Route path="/menu" element={<Menu />} />
         </Routes>
     </Router>
   );
